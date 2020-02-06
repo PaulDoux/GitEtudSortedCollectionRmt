417b022a4b8769dcd1b3ac92ad6abaeff372ca00
zaIhany <moi@rin.com>
2020-02-06 17:07:58 +0100

    modif encore 4

--- src/pCompar/PlayerRankingComparator.java
+++ src/pCompar/PlayerRankingComparator.java
@@ -7,4 +7,5 @@ public class PlayerRankingComparator implements Comparator<Player>{
 	public int compare(Player firstPlayer, Player secondPlayer){
 		return (firstPlayer.getRanking()-secondPlayer.getRanking());
 	}
+	//modif02
 }
---
+++ src/pCompar/Testclass010.java
@@ -0,0 +1,10 @@
+package pCompar;
+
+public class Testclass010 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
